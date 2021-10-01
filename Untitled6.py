f888d4a3bc8b0fff2b5fd5bb2e2b7c1e98137e91
RishiBhaisare <91720327+RishiBhaisare@users.noreply.github.com>
2021-10-01 13:06:54 +0530

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 2
+y = 5
+xy = 'Hey'
+print (x+y, xy)
+
+
+# In[3]:
+
+
+print("""My name is Rishi Bhaisare.
+
+I love Python.""")
+
+
+# In[4]:
+
+
+string1 = 'World'
+print('Hello', string1)
+
+
+# In[5]:
+
+
+string2 = '!'
+print('Hello', string1, string2)
+
+
+# In[7]:
+
+
+print('Hello' +  string1 + string2)
+
+
+# In[9]:
+
+
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
+
+# In[10]:
+
+
+print("I want \\n to be printed.")
+
+
+# In[17]:
+
+
+s = 'abc'
+
+x=100
+print (type(x)) 
+print (type(s))
+ 
+ 
+value= True
+print(type(value))
+
+percent=90.00
+type(percent)
+
+
+# In[ ]:
+
+
+
+
